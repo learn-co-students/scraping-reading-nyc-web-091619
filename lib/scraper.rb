356c98371448137af6c59b9707674318ccdccbb4
Huseyin Cicek <htcicek@users.noreply.github.com>
2019-09-24 15:38:18 -0400

    went through code along

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,4 +1,10 @@
 require 'nokogiri'
 require 'open-uri'
+require 'pry'
 
 html = open("https://flatironschool.com/")
+
+binding.pry
+
+doc = Nokogiri::html(html)
+doc.css(".headline-260IBN").text.strip
